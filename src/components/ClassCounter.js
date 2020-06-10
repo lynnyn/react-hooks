1e8325121cb471955f1abbb55d41ceb0ab315d4a
Lynn Yuan <lynnyuann@gmail.com>
2020-06-11 10:53:16 +1200

    useState withprevious state,object and array

---
+++ src/components/ClassCounter.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+class ClassCounter extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             count:0
+        }
+    this.incrementCount = this.incrementCount.bind(this);
+    }
+
+    
+
+   incrementCount() {
+    this.setState((prevState) => ({
+      count: prevState.count + 1
+    }))
+}
+    render() {
+        return (
+            <div>
+                <button onClick = {this.incrementCount}>Count {this.state.count}</button>
+            </div>
+        )
+    }
+}
+
+export default ClassCounter
